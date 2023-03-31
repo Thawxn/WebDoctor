cbe36f734f93e4bb9b987a7edb7d9a6c7875a5d3
Thawan Marques <thawan28032001@gmail.com>
2023-03-30 21:19:06 -0300

    Setup do Projeto

---
+++ index.js
@@ -0,0 +1,19 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/data', {useNewUrlParser: true, useUnifiedTopology: true})
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('Hello Word!')
+})
+
+app.listen(8080, () => {})
